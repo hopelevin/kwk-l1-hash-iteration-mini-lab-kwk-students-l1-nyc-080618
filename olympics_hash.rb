0be8151befea0bb62349e61cdcbb440828be3e11
hopelevin <hopealevin@gmail.com>
2018-08-10 14:17:55 +0000

    automatically backed up by learn

---
+++ olympics_hash.rb
@@ -0,0 +1,28 @@
+def create_olympics_hash
+  olympics {:Sydeny => "2000" ,
+  :Athens => "2004" , 
+  :Beijing => "2008" ,
+  :London => "2012"}
+end
+create_olympics_hash
+  new_olympics = create_olympics_hash
+  new_olympics = [:Atlanta] = 1996
+def add_a_key_value_pair
+end
+
+def iterate_through_hash
+  # olympics = [:Atlanta, :Sydeny , :Athens , :Beijing , :London]
+  # year = [1996, 2000, 2004, 2008, 2012]
+  # counter = 1996
+  # olympics.each do |olympics|
+  #   olympics_hash[olympics] = year[counter]
+  #   counter += 1
+
+puts "The "
+
+end
+puts iterate_through_hash
+
+def iterate_through_keys
+  
+end
